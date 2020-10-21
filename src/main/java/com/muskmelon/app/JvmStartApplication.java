df9a93745d96a7b5154cc49e2ca980275e97228e
chenzhenxiang <chenzhenxiang@jd.com>
2020-10-21 20:26:31 +0800

    Initial commit

---
+++ src/main/java/com/muskmelon/app/JvmStartApplication.java
@@ -0,0 +1,26 @@
+package com.muskmelon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <pre>
+ * 功能说明：
+ * </pre>
+ *
+ * @author muskmelon
+ * @date 2020/10/21 20:17
+ * @since 1.0
+ */
+@SpringBootApplication
+public class JvmStartApplication {
+
+    /**
+     * 应用程序启动方法
+     *
+     * @param args 启动参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(JvmStartApplication.class, args);
+    }
+}
